fb220305237be42c4b7594c8d4f65799bc54fe98
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-05-01 14:23:02 +0900

    논리 오류 아직 해결 못함
    class부분 tf.tensor로 뜨는거 마저 수정해야함

--- Traffic-sign-Test.py
+++ Traffic-sign-Test.py
@@ -37,48 +37,48 @@ def preprocessing(img):
 
 def getClassName(classNo):
     if   classNo == 0: return 'speed limit 20 km/h'
-    elif classNo == 1: return
-    elif classNo == 2: return
-    elif classNo == 3: return
-    elif classNo == 4: return
-    elif classNo == 5: return
-    elif classNo == 6: return
-    elif classNo == 7: return
-    elif classNo == 8: return
-    elif classNo == 9: return
-    elif classNo == 10: return
-    elif classNo == 11: return
-    elif classNo == 12: return
-    elif classNo == 13: return
-    elif classNo == 14: return
-    elif classNo == 15: return
-    elif classNo == 16: return
-    elif classNo == 17: return
-    elif classNo == 18: return
-    elif classNo == 19: return
-    elif classNo == 20: return
-    elif classNo == 21: return
-    elif classNo == 22: return
-    elif classNo == 23: return
-    elif classNo == 24: return
-    elif classNo == 25: return
-    elif classNo == 26: return
-    elif classNo == 27: return
-    elif classNo == 28: return
-    elif classNo == 29: return
-    elif classNo == 30: return
-    elif classNo == 31: return
-    elif classNo == 32: return
-    elif classNo == 33: return
-    elif classNo == 34: return
-    elif classNo == 35: return
-    elif classNo == 36: return
-    elif classNo == 37: return
-    elif classNo == 38: return
-    elif classNo == 39: return
-    elif classNo == 40: return
-    elif classNo == 41: return
-    elif classNo == 42: return
+    elif classNo == 1: return 'Speed Limit 30 km/h'
+    elif classNo == 2: return 'Speed Limit 50 km/h'
+    elif classNo == 3: return 'Speed Limit 60 km/h'
+    elif classNo == 4: return 'Speed Limit 70 km/h'
+    elif classNo == 5: return 'Speed Limit 80 km/h'
+    elif classNo == 6: return 'End of Speed Limit 80 km/h'
+    elif classNo == 7: return 'Speed Limit 100 km/h'
+    elif classNo == 8: return 'Speed Limit 120 km/h'
+    elif classNo == 9: return 'No passing'
+    elif classNo == 10: return 'No passing for vechiles over 3.5 metric tons'
+    elif classNo == 11: return 'Right-of-way at the next intersection'
+    elif classNo == 12: return 'Priority road'
+    elif classNo == 13: return 'Yield'
+    elif classNo == 14: return 'Stop'
+    elif classNo == 15: return 'No vechiles'
+    elif classNo == 16: return 'Vechiles over 3.5 metric tons prohibited'
+    elif classNo == 17: return 'No entry'
+    elif classNo == 18: return 'General caution'
+    elif classNo == 19: return 'Dangerous curve to the left'
+    elif classNo == 20: return 'Dangerous curve to the right'
+    elif classNo == 21: return 'Double curve'
+    elif classNo == 22: return 'Bumpy road'
+    elif classNo == 23: return 'Slippery road'
+    elif classNo == 24: return 'Road narrows on the right'
+    elif classNo == 25: return 'Road work'
+    elif classNo == 26: return 'Traffic signals'
+    elif classNo == 27: return 'Pedestrians'
+    elif classNo == 28: return 'Children crossing'
+    elif classNo == 29: return 'Bicycles crossing'
+    elif classNo == 30: return 'Beware of ice/snow'
+    elif classNo == 31: return 'Wild animals crossing'
+    elif classNo == 32: return 'End of all speed and passing limits'
+    elif classNo == 33: return 'Turn right ahead'
+    elif classNo == 34: return 'Turn left ahead'
+    elif classNo == 35: return 'Ahead only'
+    elif classNo == 36: return 'Go straight or right'
+    elif classNo == 37: return 'Go straight or left'
+    elif classNo == 38: return 'Keep right'
+    elif classNo == 39: return 'Keep left'
+    elif classNo == 40: return 'Roundabout mandatory'
+    elif classNo == 41: return 'End of no passing'
+    elif classNo == 42: return 'End of no passing by vechiles over 3.5 metric tons'
 
 while True:
     
@@ -86,23 +86,25 @@ while True:
     success, imgOrignal = cap.read()
     
     # process image
-    img = np.array(imgOrignal)
+    img = np.asarray(imgOrignal)
     img = cv2.resize(img, (32, 32))
     img = preprocessing(img)
     cv2.imshow("processed Image", img)
     img = img.reshape(1, 32, 32, 1)
     cv2.putText(imgOrignal, "CLASS: ", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.putText(imgOrignal, "PROBABILITY: ", (20, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(imgOrignal, "PROBABILITY: ", (20, 75), font, 0.75, (255, 0, 0), 2, cv2.LINE_AA)
     
     #predict image
     predictions = model.predict(img)
     classIndex = model.predict_step(img)
     probabilityValue = np.amax(predictions)
     if probabilityValue > threshold:
-        # print(getClassname(classindex))
-        cv2.putText(imgOrignal, str(classIndex)+" "+str(getClassName(classIndex)), (120, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
-        cv2.putText(imgOrignal, str(round(probabilityValue*100, 2))+"%", (180, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+        # print(getClassName(classIndex))
+        cv2.putText(imgOrignal, str(classIndex), (120, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(imgOrignal, str(round(probabilityValue*100, 2))+"%", (180, 75), font, 0.75, (255, 0, 0), 2, cv2.LINE_AA)
     cv2.imshow("Result", imgOrignal)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# +" "+str(getClassName(classIndex))
