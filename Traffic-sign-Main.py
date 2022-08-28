2d19ad0641fce7a91100083ffda5bf4b43cc14ea
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-08-28 18:02:51 +0900

    Update

--- Traffic-sign-Main.py
+++ Traffic-sign-Main.py
@@ -1,24 +1,25 @@
+import os
+import cv2
+import random
+import pickle
+import pandas as pd
 import numpy as np
+from keras.layers import Dense
 import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
-from keras.utils.np_utils import to_categorical
+from keras.models import load_model
 from keras.layers import Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
-import cv2
-from sklearn.model_selection import train_test_split
-import os
-import pandas as pd
-import random
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import load_model
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import Adam
+from keras.utils.np_utils import to_categorical
 
 
-# parameters
-path = "myData" # folder with all the class folders
-labelFile = 'labels.csv' # file with all names of classes
-batch_size_val = 50 # how manu to process together
+# 파라미터
+path = "myData" # 이미지 파일 담겨있는 폴더
+labelFile = 'labels.csv' # 라벨명 담겨있는 폴더
+batch_size_val = 50 
 steps_per_epoch_val = 100
 epochs_val =10
 imageDimesions = (32, 32, 3)
@@ -26,7 +27,7 @@ testRatio = 0.2
 validationRatio = 0.2
 ###############################################################
 
-# Importing of the Images
+# 이미지 임포트 해오기
 count = 0
 images = []
 classNo = []
@@ -45,10 +46,11 @@ for x in range(0, len(myList)):
     print(count, end=" ")
     count +=1
 print(" ")
+# 이미지 넘파이 배열로 바꿔주기
 images = np.array(images)
 classNo = np.array(classNo)
 
-# split data
+# 데이터 나눠주기
 x_train, x_test, y_train, y_test = train_test_split(images, classNo, test_size=testRatio)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=validationRatio)
 
@@ -57,7 +59,7 @@ x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=va
 
 ###################################################################
 
-# to check id number of images matches to number of lables for each data set
+# 이미지의 id 갯수랑 라벨갯수 맞는지 확인 
 print("Data Shapes")
 print("Train", end="");print(x_train.shape, y_train.shape)
 print("Validation", end="");print(x_val.shape, y_val.shape)
@@ -70,11 +72,11 @@ assert(x_val.shape[1:]==(imageDimesions))
 assert(x_test.shape[1:]==(imageDimesions))
 
 ######################################################################
-# Read CSV File
+# CSV File 읽기
 data=pd.read_csv(labelFile)
 print("data shape", data.shape, type(data))
 
-# Display some samples images of all the classes
+# 모든 클래스의 이미지 파일 보여주기
 num_of_samples = []
 cols = 5
 num_classes = noOfClasses
@@ -89,7 +91,7 @@ for i in range(cols):
             axs[j][i].set_title(str(j)+"-"+row["Name"])
             num_of_samples.append(len(x_selected))
             
-# Display a bar chart showing no of samples for each category
+# 카테고리별 바차트
 print(num_of_samples)
 plt.figure(figsize=(12, 4))
 plt.bar(range(0, num_classes), num_of_samples)
@@ -98,7 +100,7 @@ plt.xlabel("Class number")
 plt.ylabel("Number of images")
 plt.show()
 
-# preprocessing the images
+# 전처리(그레이스케일, 표준화, 정규화)
 def grayscale(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
@@ -116,14 +118,14 @@ def preprocessing(img):
 x_train= np.array(list(map(preprocessing, x_train)))
 x_val = np.array(list(map(preprocessing, x_val)))
 x_test = np.array(list(map(preprocessing, x_test)))
-cv2.imshow("GrayScale Images", x_train[random.randint(0, len(x_train)-1)]) # to check if the training is done properly
+cv2.imshow("GrayScale Images", x_train[random.randint(0, len(x_train)-1)]) 
 
-# add a depth of 1
+# 1차원 추가하여 쉐이프 변화 시키기
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
 x_val = x_val.reshape(x_val.shape[0], x_val.shape[1], x_val.shape[2], 1)
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 
-# Augmentation of images: to makeit more generic
+# 이미지 데이터 증강
 dataGen = ImageDataGenerator(width_shift_range= 0.1,
                                 height_shift_range= 0.1,
                                 zoom_range= 0.2,
@@ -134,7 +136,7 @@ dataGen.fit(x_train)
 batches = dataGen.flow(x_train, y_train, batch_size=20)
 x_batch, y_batch = next(batches)
 
-# to show agmented image samples
+# 데이터 증강된 이미지 보여주기
 fig, axs = plt.subplots(1, 15, figsize=(20, 5))
 fig.tight_layout()
 
@@ -147,18 +149,19 @@ y_train = to_categorical(y_train, noOfClasses)
 y_val = to_categorical(y_val, noOfClasses)
 y_test = to_categorical(y_test, noOfClasses)
 
-# convolution neural network model
+# 커스텀 CNN 모델 생성
 def myModel():
     no_Of_Fileters = 60
-    size_of_Filter = (5, 5) # this is the kernel that move around the image to get the features.
+    size_of_Filter = (5, 5) 
     
     size_of_Filters2 = (3, 3)
     size_of_pool=(2, 2)
     no_Of_Nodes = 500
+    
     model = Sequential()
     model.add((Conv2D(no_Of_Fileters, size_of_Filter, input_shape=(imageDimesions[0], imageDimesions[1], 1), activation='relu')))
     model.add((Conv2D(no_Of_Fileters, size_of_Filter, activation='relu')))
-    model.add(MaxPooling2D(pool_size=size_of_pool)) # does not effect the depth/no of filters
+    model.add(MaxPooling2D(pool_size=size_of_pool)) 
     
     model.add((Conv2D(no_Of_Fileters//2, size_of_Filters2, activation='relu')))
     model.add((Conv2D(no_Of_Fileters//2, size_of_Filters2, activation='relu')))
@@ -167,19 +170,19 @@ def myModel():
     
     model.add(Flatten())
     model.add(Dense(no_Of_Nodes, activation='relu'))
-    model.add(Dropout(0.5)) # inputs nodes to drop with each update 1 all 0 none
-    model.add(Dense(noOfClasses, activation='softmax')) # output layer
+    model.add(Dropout(0.5)) 
+    model.add(Dense(noOfClasses, activation='softmax'))
     
-    # compile model
+    # CNN model 컴파일
     model.compile(Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-# train
+# 훈련
 model = myModel()
 print(model.summary())
 history = model.fit_generator(dataGen.flow(x_train, y_train, batch_size=batch_size_val), steps_per_epoch=steps_per_epoch_val, epochs= epochs_val, validation_data=(x_val, y_val), shuffle=1) 
 
-# plot
+# History 
 plt.figure(1)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -199,7 +202,7 @@ score = model.evaluate(x_test, y_test, verbose=0)
 print('Test Score:', score[0])
 print('Test Accuracy:', score[1])
 
-# store the model as a pickle object
+# 모델 피클파일로 저장
 # pickle_out = open("./model_trained.txt", "wb")
 # pickle.dump(model, pickle_out)
 # pickle_out.close()
